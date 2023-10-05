a3373838e768f99da16884aaf81c8ea5a4e02a84
LucaVas <luca.vassos@gmail.com>
2023-10-05 21:27:12 +0300

    Add houses route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import applicationsRouter from './routes/applications/applications.js';
 import applicantsRouter from './routes/applicants/applicants.js';
+import housesRouter from './routes/houses/houses.js';
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use('/api/v1/applications', applicationsRouter);
 app.use('/api/v1/applicants', applicantsRouter);
+app.use('/api/v1/houses', housesRouter);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
---
+++ backend/routes/houses/houses.js
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+import db from '../../data/index.js';
+
+const housesRouter = Router();
+
+const houses = {
+  houses: [
+    { id: 0, address: 'Address', available: true },
+    { id: 1, address: 'Address 1', available: false },
+    { id: 2, address: 'Address 2', available: false },
+  ],
+  error: {
+    code: 404,
+    message: 'House not found',
+  },
+};
+
+housesRouter.route('/').get((req, res) => {
+  if (houses.houses.length > 0) {
+    res.send(houses.houses);
+  } else {
+    res.send(houses.error);
+  }
+});
+
+housesRouter.route('/:id').get((req, res) => {
+  const found_house = houses.houses[parseInt(req.params.id)];
+  if (found_house) {
+    res.send(found_house);
+  } else {
+    res.send(houses.error);
+  }
+});
+
+export default housesRouter;
