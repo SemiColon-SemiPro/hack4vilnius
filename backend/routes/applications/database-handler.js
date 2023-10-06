412132c91253258b7988467b6e9186e7ad45bf8f
LucaVas <luca.vassos@gmail.com>
2023-10-06 10:58:59 +0300

    [3] Handlers and methods set, test: OK

--- backend/routes/applications/database-handler.js
+++ backend/routes/applications/database-handler.js
@@ -6,9 +6,13 @@ const SELECT_APPLICATION_BY_ID = 'SELECT * FROM applications WHERE id = ?';
 
 // functions
 export const getApplications = () => {
-  db.exec(SELECT_APPLICAITONS);
+  const applications = db.prepare(SELECT_APPLICAITONS).all();
+  console.debug('Number of applications retrieved: ', applications.length);
+  return applications;
 };
 
 export const getApplicationById = id => {
-  db.prepare(SELECT_APPLICATION_BY_ID).get(id);
+  const application = db.prepare(SELECT_APPLICATION_BY_ID).get(id);
+  console.debug(`Application with id ${id} retrieved: `, application);
+  return application;
 };
