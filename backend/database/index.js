22e52b0ac2d26ade7302198496ae603856fa1189
LucaVas <luca.vassos@gmail.com>
2023-10-06 10:24:38 +0300

    Add migration folder and remove scripts

--- backend/database/index.js
+++ backend/database/index.js
@@ -20,7 +20,7 @@ const createDb = dbPath => {
 
 const createTables = async database => {
   const createTablesSql = await readSqlScript(
-    './database/scripts/create-tables.sql'
+    './database/migration/create-tables.sql'
   );
   createTablesSql.forEach(stmt => database.exec(stmt));
 };
