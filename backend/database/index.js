20972476276d92aff82ce85b7bc75bc6d7f83afc
s p <prietosebby@gmail.com>
2023-10-07 09:44:56 +0300

    do not merge

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,12 +1,12 @@
-import sqlite from 'better-sqlite3';
-import createTables from './migration/createTables/index.js';
+import sqlite from 'better-sqlite3'
+import createTables from './migration/createTables/index.js'
 
-const createDb = dbPath => {
-  const db = sqlite(dbPath);
-  return db;
-};
+const createDb = (dbPath) => {
+    const db = sqlite(dbPath)
+    return db
+}
 
-const db = createDb('./database/database.db');
-createTables(db);
+const db = createDb('./database/database.db')
+createTables(db)
 
-export default db;
+export default db
