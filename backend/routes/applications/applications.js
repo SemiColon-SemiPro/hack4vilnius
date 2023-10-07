dbdbc57a508ba99fbcfda522d72415041410760c
s p <prietosebby@gmail.com>
2023-10-07 16:26:08 +0300

    app routes

--- backend/routes/applications/applications.js
+++ backend/routes/applications/applications.js
@@ -25,20 +25,20 @@ applicationsRouter.route("/").get((req, res) => {
 		if (applicationList.length !== 0) {
 			res.status(200).json({ applications: applicationList });
 		}
-	}
-
-	applicationList = getNumberOfApplicants(desiredNumApplicants);
-
-	if (applicationList.length === 0) {
-		res.status(404).json({
-			error: {
-				code: 404,
-				message:
-					"No applications found with the specified number of applicants",
-			},
-		});
 	} else {
-		res.status(200).json({ applications: applicationList });
+		applicationList = getNumberOfApplicants(desiredNumApplicants);
+
+		if (applicationList.length === 0) {
+			res.status(404).json({
+				error: {
+					code: 404,
+					message:
+						"No applications found with the specified number of applicants",
+				},
+			});
+		} else {
+			res.status(200).json({ applications: applicationList });
+		}
 	}
 });
 
