62b37e8cc3e76f82d6c7350a210d4012349b3559
s p <prietosebby@gmail.com>
2023-10-07 16:13:42 +0300

    application routes

--- backend/routes/applications/applications.js
+++ backend/routes/applications/applications.js
@@ -9,33 +9,22 @@ import {
 
 const applicationsRouter = Router();
 
-// applicationsRouter.route("/").get((req, res) => {
-// 	const applicantionList = getApplications();
-// 	if (applicantionList.length === 0) {
-// 		res.status(404).json({
-// 			error: {
-// 				code: 404,
-// 				message: "No applications found in the database",
-// 			},
-// 		});
-// 	}
-// 	if (applicantionList.length !== 0) {
-// 		res.status(200).json({ applications: applicantionList });
-// 	}
-// });
-
 applicationsRouter.route("/").get((req, res) => {
 	const desiredNumApplicants = parseInt(req.query.group);
 
 	if (!desiredNumApplicants) {
-		res.status(400).json({
-			error: {
-				code: 400,
-				message:
-					"Please provide the desired number of applicants as a query parameter (num_applicants).",
-			},
-		});
-		return;
+		const applicantionList = getApplications();
+		if (applicantionList.length === 0) {
+			res.status(404).json({
+				error: {
+					code: 404,
+					message: "No applications found in the database",
+				},
+			});
+		}
+		if (applicantionList.length !== 0) {
+			res.status(200).json({ applications: applicantionList });
+		}
 	}
 
 	const applicationsList = getNumberOfApplicants(desiredNumApplicants);
@@ -63,47 +52,5 @@ applicationsRouter.route("/new").put(async (req, res) => {
 		res.status(400).send(e.message);
 	}
 });
-
-// applicationsRouter
-// 	.route("/:id")
-// 	.get((req, res) => {
-// 		const data = {
-// 			queryParams: req.query,
-// 			id: parseInt(req.params.id),
-// 		};
-
-// 		const application = getApplicationById(data.id);
-
-// 		if (!application)
-// 			res.status(404).send(`Application with id ${data.id} not found`);
-// 		res.send(application);
-// 	})
-// 	.post(async (req, res) => {
-// 		const id = parseInt(req.params.id);
-// 		const application = db
-// 			.prepare("SELECT * FROM applications WHERE id = ?")
-// 			.get([id]);
-// 		// check if exists
-// 		if (!application) {
-// 			res.status(404).send(
-// 				"Application cannot be updated because it does not exists",
-// 			);
-// 			return;
-// 		}
-
-// 		try {
-// 			const result = await applicationsSchema.validateAsync(req.body);
-// 			// update application in db
-// 			res.status(200).send(result);
-// 		} catch (e) {
-// 			console.error(e);
-// 			res.status(400).send(e.message);
-// 		}
-// 	})
-// 	.delete((req, res) => {
-// 		const id = req.params.id;
-// 		db.prepare("DELETE FROM applications WHERE id = ?").run(id);
-// 		res.send(`Application with id ${id} deleted`);
-// 	});
 
 export default applicationsRouter;
