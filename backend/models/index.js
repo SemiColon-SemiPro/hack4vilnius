638f9d98bcd924e5b67af77951712c3c1156a795
Luca Vassos <luca.vassos@gmail.com>
2023-10-08 12:45:16 +0300

    add elderly score calculation

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,10 +1,19 @@
-import { addDisabilityScore } from "./database_handler.js";
+import { addScore } from "./database_handler.js";
 import calculateDisabilityScore from "./disability/index.js";
+import calculateElderlyScore from "./elderly/index.js";
 
 const calculateScore = (applicationId) => {
-	const score = calculateDisabilityScore(applicationId);
+	let score = 0.0;
+
+	const disabilityScore = calculateDisabilityScore(applicationId);
+	console.info(`INFO >> Disability score calculated ${disabilityScore}`);
+	const elderlyScore = calculateElderlyScore(applicationId);
+	console.info(`INFO >> Elderly score calculated ${elderlyScore}`);
+
+	score = disabilityScore + elderlyScore;
 	console.info(`INFO >> Score calculated ${score}`);
-	addDisabilityScore(score, applicationId);
+
+	addScore(score, applicationId);
 };
 
 export default calculateScore;
